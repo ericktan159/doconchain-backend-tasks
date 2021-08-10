126299fe57fe29f986589a3a8d7f543853bd3988
unknown <nlfajardo06@gmail.com>
2021-08-10 15:34:49 +0800

    First commit

---
+++ verify-post/index.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+
+// Insert URL here
+const url = "https://www.facebook.com/Doconchain/posts/339575264352883";
+
+axios
+  .request(url)
+  .then((res) => {
+    const data = res.data;
+
+    if (!data.includes("/hashtag/doconchain?")) {
+      return console.log("Post does not contain #DocOnChain!");
+    }
+
+    console.log("Post contains #DocOnChain!");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
